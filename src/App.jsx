a5a25eb6717e7ab5833ff2bece6bae910fc7e2ba
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-29 01:08:31 -0600

    Added header and footer components.  Tested and working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import './App.css'
-import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import {Home, Portfolio, Contact, Resume} from"./pages"
 
 
@@ -13,9 +13,9 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/portfolio" element={<Portfolio />} />
+      {/* <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/resume" element={<Resume />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/contact" element={<Contact />} /> */}
       <Route path="/*" element={<Home />} />
     </Routes>
     </BrowserRouter>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+export default function Home() {
+
+  
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
