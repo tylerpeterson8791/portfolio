427a618a743ad5aaed9281383b2d91ffba954b84
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-03-01 00:18:44 -0600

    Created generic resume page.  Functionality draft complete.  Pushing branch.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,25 @@
-export default function Resume() {
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import resumeImage from '../assets/images/resume.jpg'; // Adjust the path based on your project structure
+// import './Resume.css'; // Create a separate CSS file for styling.  MAYBE???
 
-  
-    return (
-      <>
-       
-      </>
-    )
-  }
+const Resume = () => {
+  return (
+    <div className="resume-page">
+      <Header />
+      <Container className="fluid content-container">
+        <Row>
+          <Col xs={12}>
+            <img src={resumeImage} alt="Resume" className="resume-image" />
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default Resume;
