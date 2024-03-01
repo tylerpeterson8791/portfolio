03ab6dce711a23f957a6f4bc30e777dd8486b2e2
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-29 23:13:55 -0600

    Tweaked home some.  Moving on to portfolio.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import Header from '../components/header'
-import Footer from '../components/footer'
-import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Header from '../components/header';
+import Footer from '../components/footer';
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import marioImage from '../assets/images/marioImage.png';
+import '../App.css'
+
 
 export default function Home() {
-
-  
   return (
-    <div>
-    <Header />
-    <Container className='fluid'>
-      <Row>
-        {/* Left side with an image */}
-        <Col xs={12} md={6}>
-          <img src={marioImage} alt="Mario Image REPLACE" className="img-fluid" />
-        </Col>
-        
-        {/* Right side with a heading and text block */}
-        <Col xs={12} md={6}>
-          <Container>
-            <h1>Tyler Peterson</h1>
-            <p>
-              This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go.
-            </p>
-            <p>
-              This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go.
-            </p>
-            <p>
-              This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go. This is where a quick bio will go.
-            </p>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
-    <Footer />
-  </div>
-);
+    <div className="page-container">
+      <Header/>
+      <Container className='fluid content-container'>
+        <Row>
+          {/* Left side with an image */}
+          <Col xs={12} md={6}>
+            <img src={marioImage} alt="Mario Image REPLACE" className="img-fluid" />
+          </Col>
+
+          {/* Right side with a heading and text block */}
+          <Col xs={12} md={6}>
+            <Container>
+              <h1>Tyler Peterson</h1>
+              <p>
+                This is where a quick bio will go.   This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.
+              </p>
+              <p>
+              This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.
+              </p>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+      <Footer className="footer-stick"/>
+    </div>
+  );
 };
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import marioImage from '../assets/images/marioImage.png';
+import '../App.css'
+
 export default function Portfolio() {
 
   
     return (
-      <>
-       
-      </>
-    )
-  }
+      <div className="page-container">
+      <Header/>
+      <Container className='fluid content-container'>
+        <Row>
+          {/* Left side with an image */}
+          <Col xs={12} md={6}>
+            <img src={marioImage} alt="Mario Image REPLACE" className="img-fluid" />
+          </Col>
+
+          {/* Right side with a heading and text block */}
+          <Col xs={12} md={6}>
+            <Container>
+              <h1>Tyler Peterson</h1>
+              <p>
+                This is where a quick bio will go.   This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.
+              </p>
+              <p>
+              This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.  This is where a quick bio will go.
+              </p>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+      <Footer className="footer-stick"/>
+    </div>
+  );
+};
