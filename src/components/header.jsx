4339c5ca1be340f8d040958c94ba3b0a95b6a944
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-03-22 13:26:06 -0500

    fixed header to collapse and resume to fit screen

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" className="press-start px-4">
+    <Navbar bg="dark" variant="dark" className="press-start px-4" expand="md" collapseOnSelect>
       <Navbar.Brand>Tyler Peterson</Navbar.Brand>
-      <Nav className="ml-auto">
-        <Nav.Link as={Link} to="/">About</Nav.Link>
-        <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
-        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-        <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
-      </Nav>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/">About</Nav.Link>
+          <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
